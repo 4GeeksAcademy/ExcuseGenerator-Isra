b01a44d0c9d099dc1ab662aaf79462a0c6c2dec3
Israel Sánchez <125656412+isramagix@users.noreply.github.com>
2024-04-22 19:01:17 +0000

    Excuse Generator con boton

--- src/app.js
+++ src/app.js
@@ -4,8 +4,62 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  newExcuse();
 };
+
+const who = [
+  "My dog",
+  "My sister",
+  "My grandma",
+  "His bird",
+  "My neighbor",
+  "The delivery guy"
+];
+const action = ["ate", "crushed", "broke", "peed", "stole", "lost"];
+const what = [
+  "my homework",
+  "the keys",
+  "the umbrella",
+  "the car",
+  "my phone",
+  "my lunch",
+  "the computer"
+];
+const when = [
+  "before the class",
+  "right now",
+  "during my lunch",
+  "while I was sleeping",
+  "in the morning",
+  "at midnight",
+  "while I was at the store",
+  "during the meeting"
+];
+
+function GenereExcuse(arr1, arr2, arr3, arr4) {
+  let excuse = "";
+  let rand1 = Math.floor(Math.random() * arr1.length);
+  let rand2 = Math.floor(Math.random() * arr1.length);
+  let rand3 = Math.floor(Math.random() * arr1.length);
+  let rand4 = Math.floor(Math.random() * arr1.length);
+
+  excuse =
+    arr1[rand1] +
+    " " +
+    arr2[rand2] +
+    " " +
+    arr3[rand3] +
+    " " +
+    arr4[rand4] +
+    ".";
+  return excuse;
+}
+
+function newExcuse() {
+  document.querySelector("#btnExcuse").addEventListener("click", function() {
+    var excusaa = GenereExcuse(who, action, what, when);
+    document.querySelector("#excusa").textContent = excusaa;
+  });
+}
